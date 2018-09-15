adbd05ba87c7fc801f9662240d1f0d0b7415f999
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2018-09-15 18:15:16 -0400

    finalized creating snippets

--- api/create_snippet.py
+++ api/create_snippet.py
@@ -1,8 +1,14 @@
-from api.models import Snippet
+from api.models import Snippet, Word, Audio
+from api import models
 
-def create_snippet(word, url, starttime, endtime):
-  new_Snippet = Snippet(word=word, start=starttime, end=endtime, url=url)
+def create_snippet(word, rawUrl, url, starttime, endtime):
+  try:
+    word_object = models.Word.objects.get(value=word)
+  except models.Word.DoesNotExist:
+    word_object = Word(value=word)
+    word_object.save()
+   
+  audio_object = models.Audio.objects.get(url = rawUrl)
+  new_Snippet = Snippet(word=word_object, audio=audio_object, start=starttime, end=endtime, url=url)
   new_Snippet.save()
   print("Saved \"%s\" Snippet to database" % word)
-
-
