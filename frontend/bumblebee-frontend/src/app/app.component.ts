3c0d2fee2dfff410db58b5d2ec34697fd9333ee4
Conner Chyung <chyungspice@gmail.com>
2018-09-15 18:58:15 -0400

    start frontend

---
+++ frontend/bumblebee-frontend/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'bumblebee-frontend';
+
+  ngOnInit() {
+    $(window).resize(function () {
+      this.resizeDiv();
+    });
+
+    this.resizeDiv();
+  }
+
+  resizeDiv() {
+    let vph = $(window).height();
+    if (vph > 500) {
+      $(".auto-resize").css({"height": vph + "px"});
+    }
+  }
+}
