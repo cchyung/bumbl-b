a7c2e74d23976d125ad6cc91e1b704505779b963
Mark <mark.cd.hudson@gmail.com>
2018-09-15 21:58:50 -0700

    add gunicorn to requirements

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build  --tag us.gcr.io/bumblebee-mit/bumblebee:0.0.13 .
+docker build  --tag us.gcr.io/bumblebee-mit/bumblebee:0.0.15 .
 
-gcloud docker -- push us.gcr.io/bumblebee-mit/bumblebee:0.0.13
+gcloud docker -- push us.gcr.io/bumblebee-mit/bumblebee:0.0.15
 
 kubectl apply -f kubernetes.yml
